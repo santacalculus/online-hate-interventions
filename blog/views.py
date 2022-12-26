a5775bc3c461a0686542e33ded8cd5bc4d9917e2
santacalculus <lchintar@andrew.cmu.edu>
2022-12-26 09:11:48 -0500

    setup django

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def base_action(request) :
+    context = {}
+    if request.method == 'GET' :
+        return render(request, 'blog/pagecontent.html', context)
